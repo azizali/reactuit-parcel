77b1216964b7cee290f31817e2f6f9f239d58844
Aziz Ali <aziz@ilovecoding.org>
2019-12-12 13:23:17 -0800

    Parcel setup and folder structure

---
+++ src/Content/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// Aliasing named import
+import { Page as TimesheetsPage } from '../Timesheets';
+
+export default function Content() {
+  return (
+    <>
+      <content>I am a content</content>
+      <TimesheetsPage />
+    </>
+  );
+}
---
+++ src/Timesheets/index.js
@@ -0,0 +1,12 @@
+import Page from './components/Page';
+import Row from './components/Row';
+
+// One Main thing and others are secondary
+// export default Page;
+// export { Row };
+
+// Equal important
+export {
+  Page,
+  Row,
+};
